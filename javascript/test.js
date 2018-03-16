bbc650399089aa67be7d795523c6cbf7586c76dc
Kit <kitpang.zheng@gmail.com>
2018-03-16 15:03:24 -0400

    added some test javascript

---
+++ javascript/test.js
@@ -0,0 +1,12 @@
+var  message = "Hi! Testing short javascript code";
+alert(message);
+
+function dropdownMenu() {
+    var kitFirst = document.getElementById("dropDownClick");
+    if (kitFirst.className === "topnav") {
+        kitFirst.className += " responsive";
+        /*change topnav to topnav.responsive*/
+    } else {
+        kitFirst.className = "topnav";
+    }
+}
